ccab7c1706a971a5c593d4331ba1c604835023ed
Matthew Wilcoxson <git@akademy.co.uk>
2017-07-05 16:44:02 +0100

    Remove unneeded stuff. Improve speed.

--- example/emlo/emlo.js
+++ example/emlo/emlo.js
@@ -1,12 +1,19 @@
-var map = L.map('map').setView([39.82, -98.58], 4); //.setView([ 0,	0], 2);
+//L.mapbox.accessToken = 'pk.eyJ1IjoibW9uaWNhbXMiLCJhIjoiNW4zbEtPRSJ9.9IfutzjZrHdm2ESZTmk8Sw';
+//var map = L.mapbox.map('map', 'monicams.jpf4hpo5')
+//	.setView([
+//		0,
+//		0], 2);
 
+var map = L.map('map')
+	//.setView([39.82, -98.58], 4);
+	.setView([ 0,	0], 2);
 var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
 
 function onEachFeature(feature, layer) {
-	var count = feature.properties.count.toLocaleString();
-	layer.bindTooltip(count + " places").openTooltip();
+	//var count = feature.properties.count.toLocaleString();
+	//layer.bindTooltip(count + " places").openTooltip();
 	//layer.bindPopup(count);
 }
 
@@ -24,57 +31,21 @@ function setKeyword()
 
 function resetSolr()
 {
-	"strict mode";
+	"use strict";
 
-	var solrUrl = jQuery('#solrUrl').val();
-	var renderType = jQuery("#renderType option:selected" ).text();
-	var rptField = jQuery('#rptField').val();
-	var colorMap = jQuery('#colorMap').val();
-	var nearbyField = '';
-	var nearbyFieldType = '';
-	var popupDisplayField = '';
-	var sortField = '';
-	var showGlobalResults = false;
-
+	var colorMap = jQuery('#colorMap').val().split(',');
 	var placesData = [];
 
-	if (popupDisplayField.indexOf(',') > -1)
-		popupDisplayField = popupDisplayField.split(',');
-	else
-		popupDisplayField = [popupDisplayField];
-
-	colorMap = colorMap.split(',');
-
-	if (solr)
+	if (solr) {
 		map.removeLayer(solr);
+	}
 
-	// if the doi field is present, we format it as an html link to the jstor document
-	// first, a function to generate the html
-	var doiLinker = function(doc)
-	{
-		value = doc['doi'];
-		if (Array.isArray(value))
-			value = value.join();
-		return "<a target='_blank' href='http://www.jstor.org/stable/" + value + "'>" + value + "</a>.  ";
-	};
-	doiIndex = popupDisplayField.indexOf('doi');
-	if (doiIndex > -1 )
-		popupDisplayField[doiIndex] = ['doi', function(doc) {return doiLinker(doc);}];
-
-	var solrErrorHandler = function(jqXHR, textStatus, errorThrown)
-	{
+	var solrErrorHandler = function( jqXHR, textStatus, errorThrown ) {
 		// due to jsonp, no details are available
 		jQuery('#errorMessage').text('Solr error, bad URL or RPT field name');
 	};
 
-	var solrNearbyErrorHandler = function(jqXHR, textStatus, errorThrown)
-	{
-		// due to jsonp, no details are available
-		jQuery('#errorMessage').text('Solr error, bad URL or field name related to pop-up');
-	};
-
-	var solrSuccessHandler = function(data, textStatus, jqXHR)
-	{
+	var solrSuccessHandler = function( data, textStatus, jqXHR ) {
 		var placeNames = [];
 		placesData = [];
 		for( var i=0, iEnd=data.response.docs.length; i<iEnd; i++) {
@@ -83,55 +54,46 @@ function resetSolr()
 		}
 
 		//var nameSort = function(r,l) { return r.geonames_name.localeCompare(l.geonames_name); };
-		var reverseNameSort = function(r,l) { return r.reverseName.localeCompare(l.reverseName); };
-
+		var reverseNameSort = function( r, l ) { return r.reverseName.localeCompare(l.reverseName); };
 		placesData.sort( reverseNameSort );
 
-		for( i=0, iEnd=placesData.length; i<iEnd; i++) {
-			placeNames.push( "<option value='" + i + "'>" + placesData[i].reverseName + "  [" + placesData[i]["g"] + "]</option>" );
+		for( i=0, iEnd=placesData.length; i<iEnd; i++ ) {
+			//placeNames.push( "<option value='" + i + "'>" + placesData[i].reverseName + "  [" + placesData[i]["g"] + "]</option>" );
+			placeNames.push( ["<option value='", i, "'>", placesData[i].reverseName, "  [",placesData[i]["g"],"]</option>"].join("") );
 		}
 
-		jQuery('#placelist').html( placeNames.join(""));
-		jQuery('#errorMessage').text('');
-		jQuery('#responseTime').html('Solr response time: ' + solr.solrTime + ' ms');
-		jQuery('#numDocs').html('Number of docs: ' + solr.docsCount.toLocaleString());
-
+		jQuery( '#placelist' ).html( placeNames.join(""));
+		jQuery( '#errorMessage' ).text('');
+		jQuery( '#responseTime' ).html('Solr response time: ' + solr.solrTime + ' ms');
+		jQuery( '#numDocs' ).html('Number of docs: ' + solr.docsCount.toLocaleString());
 	};
 
-	var renderCompleteHandler = function()
-	{
-		if (solr.renderTime)
+	var renderCompleteHandler = function() {
+
+		if (solr.renderTime) {
 			$('#renderTime').html('Render time: ' + solr.renderTime + ' ms');
+		}
 	};
 
 	var keyword = jQuery('#keywordSearchText').val();
-	//http://localhost:8983/solr/jstorTest
-	solr = L.solrHeatmap(solrUrl, {
-		// Solr field with geospatial data (should be type Spatial Recursive Prefix Tree)
-		field: rptField,
-		// Sorl field needed to compute nearby items
-		nearbyField: nearbyField,
-		nearbyFieldType: nearbyFieldType,
 
-		// Set type of visualization. Allowed types: 'geojsonGrid', 'clusters' Note: 'clusters' requires LeafletMarkerClusterer, heatmap
-		type: renderType,
-		colors: colorMap, //['#000000', '#0000df', '#00effe', '#00ff42', '#feec30', '#ff5f00', '#ff0000'],
+	solr = L.solrHeatmap( "http://localhost:8983/solr/locations", {
+
+		field: "geo_rpt",
+
+		type: "geojsonGrid",
+		colors: colorMap,
 		maxSampleSize: 400,
-		popupDisplay: popupDisplayField,
-		// we optionally sort display of nearby items from smallest to largest
-		sortField: sortField,
+
 		solrErrorHandler: solrErrorHandler,
-		solrNearbyErrorHandler: solrNearbyErrorHandler,
 		solrSuccessHandler: solrSuccessHandler,
-		solrNearbySuccessHandler: solrSuccessHandler,
 		renderCompleteHandler: renderCompleteHandler,
+
 		popupHighlight: true,
-		showGlobalResults: showGlobalResults,
+		showGlobalResults: false,
 		fixedOpacity: 100,
 		filterQuery: keyword,
-		// Inherited from L.GeoJSON
-		// onEachFeature: onEachFeature,
-		//
+
 		limitFields: [
 			'g:geo',
 			'n:geonames_name',
@@ -145,8 +107,8 @@ function resetSolr()
 	solr.addTo(map);
 
 	var markerHighlight = null;
-	jQuery('#placelist').on("change",function () {
-		var index = jQuery('#placelist').val();
+	jQuery( '#placelist' ).on( "change", function () {
+		var index = jQuery( '#placelist' ).val();
 		var placeData = placesData[+index];
 
 		var latlong = placeData["g"].split(",");
@@ -154,8 +116,8 @@ function resetSolr()
 			markerHighlight = L.popup({
 				autoPan: false
 			})
-			.setLatLng([+latlong[0], +latlong[1]])
-			.openOn(map);
+			.setLatLng( [ +latlong[0], +latlong[1] ] )
+			.openOn( map );
 		}
 
 		var title = placeData["n"].split(",").slice(0,-1).join(", ");
@@ -178,11 +140,10 @@ function resetSolr()
 			'<a href="' + url + '" target="_blank">Link to main record</a>';
 
 		markerHighlight
-			.setLatLng([+latlong[0], +latlong[1]])
-			.setContent(content);
+			.setLatLng( [ +latlong[0], +latlong[1] ] )
+			.setContent( content );
 
 	});
-
 }
 
 var solr = null;
